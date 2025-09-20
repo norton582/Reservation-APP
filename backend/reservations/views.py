2af4c0ca4cfe9c52de7a865ce0367b7654793eef
norton582 <norton.rakotovao@gmail.com>
2025-09-20 23:28:44 +0300

    Ajout API REST pour chambre crud

--- backend/reservations/views.py
+++ backend/reservations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
+class ChambreViewSet(viewsets.ModelViewSet):
+    queryset = Chambre.objects.all()
+    serializer_class = ChambreSerializer
